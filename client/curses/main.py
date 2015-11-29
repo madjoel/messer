1bb14d83fb5b8108edf3ba1ba3917b57b26ded15
Matthieu Laqua <matthieu.laqua@hs-furtwangen.de>
2015-11-29 22:29:35 +0100

    Implemented main entrypoint and minimal argparse

--- client/curses/main.py
+++ client/curses/main.py
@@ -5,9 +5,32 @@
 # @author Matthieu Laqua
 # -------------------------
 
+# imports
+import sys
+from client import Client
+
 # main function
 def main():
-    pass
+    # default values for login
+    user = "nameless"
+    host = "localhost"
+    port = 13370
+
+    # parse arguments
+    for arg in sys.argv:
+        if arg.startswith("-u="):
+            user = arg[3:]
+        elif arg.startswith("-h="):
+            host = arg[3:]
+        elif arg.startswith("-p="):
+            try: port = int(arg[3:])
+            except ValueError:
+                print("Invalid port: " + arg[3:])
+                return
+
+    # start the client
+    client = Client(user, host, port)
+    client.launch()
 
 # entrypoint
 if __name__ == "__main__":
